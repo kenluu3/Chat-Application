510202844a0d36d1100458777d7e45b95f9ca313
kenluu3 <ken.luuu@hotmail.com>
2022-07-17 15:24:10 -0400

    simple server implementation

--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -1,7 +1,89 @@
 #include <iostream>
+#include <string>
+#include <cstdio>
+#include <cerrno>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#define PORT "3490"
+#define MAX_CONNECTIONS 15
 
 int main(int argc, char* argv[]) {
-  std::cout << "Hello World from Server!\n";
+  addrinfo hints{}, *server{ nullptr };
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  int result{ getaddrinfo(NULL, PORT, &hints, &server) };
+
+  if (result != 0) {
+    std::cerr << "Error retrieving server information: " << gai_strerror(result) << '\n';
+    return -1;
+  }
+
+  int listener{}; 
+  addrinfo *p{ nullptr };
+
+  for (p = server; p != nullptr; p = p->ai_next) {
+    if ((listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+      perror("socket");
+      continue;
+    }
+
+    int y{1};
+    if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &y, sizeof(y)) == -1) {
+      perror("setsockopt");
+      continue;
+    }
+
+    if (bind(listener, p->ai_addr, p->ai_addrlen) == -1) {
+      close(listener);
+      perror("bind");
+      continue;
+    }
+
+    char ipAddress[INET_ADDRSTRLEN];
+    inet_ntop(p->ai_family, p->ai_addr, ipAddress, p->ai_addrlen);
+
+    std::cout << "Successfully created socket and binded to local address: " << ipAddress << " on port " << PORT << '\n'; 
+    break;
+  }
+
+  if (p == nullptr) {
+    std::cout << "Failed to create and bind socket.\n";
+    return -2;
+  }
+
+  freeaddrinfo(server);
+
+  if (listen(listener, MAX_CONNECTIONS) == -1) {
+    perror("listen");
+    return -3;
+  }
+
+  std::cout << "Listening for connections...\n";
+
+  sockaddr_in client{};
+  socklen_t clientsize{ sizeof(client) };
+  int comm{ accept(listener, reinterpret_cast<sockaddr *>(&client), &clientsize) };
+
+  if (comm == -1) {
+    perror("accept");
+    return -4;
+  }
+
+  char clientAddress[INET_ADDRSTRLEN];
+  inet_ntop(AF_INET, &(client.sin_addr), clientAddress, INET_ADDRSTRLEN);
+  std::cout << "Client connected: " << clientAddress << '\n';
+
+  send(comm, "Hello", 5, 0);
+
+  close(comm);
+  close(listener);
 
   return 0;
 }
